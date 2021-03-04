54696d654a6f6c74/RESTwebsite

commit 38ad6661d8646802be1eac42b8638d901d7a8662

Bug fix

RESTserver/Behavior/Singleton.py
@@ -11,9 +11,8 @@ class Singleton():
     Behavior for data that is unique.
     Can only accept PUT and GET requests.
     """
-    def __init__(self, path: str, file_name: str):
+    def __init__(self, path: str):
         self.path = path
-        self.file_name = file_name
 
         if not exists(path):
             mkdir(path)
@@ -59,8 +58,9 @@ class Singleton():
         writer.write(data_to_write)
         writer.close()
 
-    def update_file(self, file_name: str):
-        data = request.get_json()
+    def update_file(self, file_name: str, data: dict = None):
+        if data is None:
+            data = request.get_json()
 
         try:
             self.update(data, file_name)
@@ -72,8 +72,8 @@ class Singleton():
     def update_all_files(self):
         data = request.get_json()
 
-        self.update(data, self.file_name)
-        self.update(data, "md")
+        self.update_file("data", data)
+        self.update_file("md", data)
 
         return Response(status = 200)
 

Client/js/DataTypes/linker.js
@@ -1,6 +1,6 @@
 import News from "./news.js";
 import Contacts from "./contacts.js";
-import { AboutUs, Entry, Exams, Documents, FreePositions, Projects } from "./static-page.js";
+import { AboutUs, Entry, Exams, FreePositions, Projects } from "./static-page.js";
 
 export default
 {
@@ -8,7 +8,6 @@ export default
     Contacts,
     Entry,
     Exams,
-    Documents,
     FreePositions,
     Projects,
     AboutUs

Client/js/DataTypes/static-page.js
@@ -19,8 +19,6 @@ class StaticPage extends DataType
         const types = [mdInput];
 
         super(titles, types);
-        
-        this.RESTroot = "aboutus";
     }
 
     submit(reqType)
@@ -31,7 +29,7 @@ class StaticPage extends DataType
             md: {
                 content: inputData[0]
             },
-            aboutus: {
+            data: {
                 content: md(inputData[0])
             }
         }
@@ -55,24 +53,25 @@ class StaticPage extends DataType
 
 export class Entry extends StaticPage
 {
+    RESTroot = "entry";
 }
 
 export class FreePositions extends StaticPage
 {
+    RESTroot = "freepositions";
 }
 
 export class Projects extends StaticPage
 {
+    RESTroot = "projects";
 }
 
 export class Exams extends StaticPage
 {
-}
-
-export class Documents extends StaticPage
-{
+    RESTroot = "exams";
 }
 
 export class AboutUs extends StaticPage
 {
+    RESTroot = "aboutus";
 }

RESTserver/driver.py
@@ -26,27 +26,18 @@ views = [
     ),
     AboutView(
         "./data/entry",
-        "entry"
     ),
     AboutView(
         "./data/freepositions",
-        "freepositions"
     ),
     AboutView(
         "./data/projects",
-        "projects"
     ),
     AboutView(
         "./data/exams",
-        "exams"
-    ),
-    AboutView(
-        "./data/documents",
-        "documents"
     ),
     AboutView(
         "./data/aboutus",
-        "aboutus"
     )
 ]
 
@@ -57,7 +48,6 @@ names = [
     "freepositions",
     "projects",
     "exams",
-    "documents",
     "aboutus"
 ]
 

commit 62709df87f6441289233b0fd9f25a0bfdd65917c

Fixed issue caused by 38ad666

Public/js/page.js
@@ -16,6 +16,8 @@ function displayPageContent(content)
     `
 }
 async function load(){
+    const data = await getPage();
+    console.log(data);
     await getPage().then((content) => displayPageContent(content));
 }
 load();

RESTserver/Behavior/Singleton.py
@@ -35,7 +35,7 @@ class Singleton():
 
     def get_data(self):
         try:
-            data = self.get_file_data(self.file_name)
+            data = self.get_file_data("data")
         except FileNotFoundError:
             return Response(status = 404)
 